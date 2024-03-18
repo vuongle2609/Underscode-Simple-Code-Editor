5bbeac22b9d8a27944371eed8726f32dabb35f26
vuongle2609 <vuongle2609@gmail.com>
2024-03-18 16:02:40 +0700

    working monaco

--- src/main.ts
+++ src/main.ts
@@ -6,20 +6,21 @@ import { install as VueMonacoEditorPlugin } from "@guolao/vue-monaco-editor";
 import App from "./App.vue";
 import router from "./router";
 import path from "path";
-
 const app = createApp(App);
+import * as monaco from "monaco-editor";
 
 // const configFile = path.join(path.dirname(__dirname), "extraResources", "vs");
 
-const configFile = path.join(process.env.VITE_PUBLIC, "vs")
-console.log("🚀 ~ configFile:", configFile);
+const configFile = path.join(path.join(__dirname, "../dist"), "vs");
 
 app.use(createPinia());
 app.use(router);
-app.use(VueMonacoEditorPlugin, {
-  paths: {
-    vs: configFile,
-  },
-});
+// app.use(VueMonacoEditorPlugin, {
+//   monaco,
+//   // paths: {
+//   //   // vs: 'extraResources/vs',
+//   //   vs: "https://cdn.jsdelivr.net/npm/monaco-editor@0.43.0/min/vs",
+//   // },
+// });
 
 app.mount("#app");
--- src/views/FileView.vue
+++ src/views/FileView.vue
@@ -1,38 +1,48 @@
 <script setup lang="ts">
 import { useFolderStore } from "@/stores/folder";
-import { ref, shallowRef, watchEffect } from "vue";
-import fs from "fs";
-
+import { ref, onMounted, shallowRef, watchEffect } from "vue";
+// import fs from "fs";
+import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
 const folderStore = useFolderStore();
-
-const code = ref("");
-
-watchEffect(() => {
-  if (folderStore.openFile)
-    fs.readFile(folderStore.openFile, "utf8", (err, data) => {
-      if (err) {
-        console.error(err);
-        return;
-      }
-      code.value = data;
-    });
+const editor = ref();
+onMounted(() => {
+  monaco.editor.create(editor.value, {
+    value: `// Type source code in your language here...
+    class MyClass {
+      @attribute
+      void main() {
+    Console.writeln( "Hello Monarch world\n");
+  }
+}`,
+  });
 });
+// const code = ref("");
+// watchEffect(() => {
+//   if (folderStore.openFile)
+//     fs.readFile(folderStore.openFile, "utf8", (err, data) => {
+//       if (err) {
+//         console.error(err);
+//         return;
+//       }
+//       code.value = data;
+//     });
+// });
 
-const MONACO_EDITOR_OPTIONS = {
-  automaticLayout: true,
-  formatOnType: true,
-  formatOnPaste: true,
-};
+// const MONACO_EDITOR_OPTIONS = {
+//   automaticLayout: true,
+//   formatOnType: true,
+//   formatOnPaste: true,
+// };
 
-const editorRef = shallowRef();
-const handleMount = (editor: string) => (editorRef.value = editor);
+// const editorRef = shallowRef();
+// const handleMount = (editor: string) => (editorRef.value = editor);
 </script>
 
 <template>
-  <vue-monaco-editor
+  <!-- <vue-monaco-editor
     v-model:value="code"
     theme="vs-dark"
     :options="MONACO_EDITOR_OPTIONS"
     @mount="handleMount"
-  />
+  /> -->  <div id="editor" ref="editor" class="w-full h-full"></div>
 </template>
