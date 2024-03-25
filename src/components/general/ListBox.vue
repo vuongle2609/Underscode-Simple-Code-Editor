c79de02e6b1f2914673bd3ff28815038edc29d43
vuongle2609 <vuongle2609@gmail.com>
2024-03-26 00:22:51 +0700

    update popover

---
+++ src/components/general/ListBox.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Button from "@/components/general/Button.vue";
+
+const options = [
+  { label: "File" },
+  { label: "View" },
+  { label: "Edit" },
+  { label: "About" },
+];
+</script>
+
+<template>
+  <div class="flex flex-col p-1">
+    <template v-for="{ label } in options">
+      <Button>
+        <span class="text-sm text-white">{{ label }}</span></Button
+      >
+    </template>
+  </div>
+</template>
--- src/components/general/Popover.vue
+++ src/components/general/Popover.vue
@@ -1,16 +1,20 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const isOpened = ref(false);
 
 const togglePopover = () => {
   isOpened.value = !isOpened.value;
 };
+
+const dropDownClass = computed(() => [
+  "absolute top-0 z-30 rounded-md shadow-md left-full bg-bgMain",
+  isOpened.value ? "" : "hidden",
+]);
 </script>
 
 <template>
   <div class="relative">
-    <slot :click="togglePopover"></slot>
-    <div class="absolute left-full top-0 shadow-xl bg-bgMain p-2 rounded-md z-30" :class="isOpened ? '' : 'hidden'">{{ isOpened }}</div>
+    <slot :toggle="togglePopover" :isOpened :dropDownClass></slot>
   </div>
 </template>
