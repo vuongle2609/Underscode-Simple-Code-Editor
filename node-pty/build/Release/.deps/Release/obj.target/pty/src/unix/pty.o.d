e0f9a1e709aed80cdac01ff6a956ced76d98288d
vuongle2609 <vuongle2609@gmail.com>
2024-04-09 10:14:49 +0700

    update node-pty

---
+++ node-pty/build/Release/.deps/Release/obj.target/pty/src/unix/pty.o.d
@@ -0,0 +1,159 @@
+cmd_Release/obj.target/pty/src/unix/pty.o := g++ -o Release/obj.target/pty/src/unix/pty.o ../src/unix/pty.cc '-DNODE_GYP_MODULE_NAME=pty' '-DUSING_UV_SHARED=1' '-DUSING_V8_SHARED=1' '-DV8_DEPRECATION_WARNINGS=1' '-DV8_DEPRECATION_WARNINGS' '-DV8_IMMINENT_DEPRECATION_WARNINGS' '-D_GLIBCXX_USE_CXX11_ABI=1' '-DELECTRON_ENSURE_CONFIG_GYPI' '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-DUSING_ELECTRON_CONFIG_GYPI' '-DV8_COMPRESS_POINTERS' '-DV8_COMPRESS_POINTERS_IN_SHARED_CAGE' '-DV8_ENABLE_SANDBOX' '-DV8_31BIT_SMIS_ON_64BIT_ARCH' '-D__STDC_FORMAT_MACROS' '-DOPENSSL_NO_PINSHARED' '-DOPENSSL_THREADS' '-DOPENSSL_NO_ASM' '-DBUILDING_NODE_EXTENSION' -I/home/vuongle/.electron-gyp/26.6.10/include/node -I/home/vuongle/.electron-gyp/26.6.10/src -I/home/vuongle/.electron-gyp/26.6.10/deps/openssl/config -I/home/vuongle/.electron-gyp/26.6.10/deps/openssl/openssl/include -I/home/vuongle/.electron-gyp/26.6.10/deps/uv/include -I/home/vuongle/.electron-gyp/26.6.10/deps/zlib -I/home/vuongle/.electron-gyp/26.6.10/deps/v8/include -I../../node_modules/nan  -fPIC -pthread -Wall -Wextra -Wno-unused-parameter -m64 -Wall -O3 -fno-omit-frame-pointer -fno-rtti -fno-exceptions -std=gnu++17 -MMD -MF ./Release/.deps/Release/obj.target/pty/src/unix/pty.o.d.raw  -std=c++17 -c
+Release/obj.target/pty/src/unix/pty.o: ../src/unix/pty.cc \
+ ../../node_modules/nan/nan.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/node_version.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/uv.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/uv/errno.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/uv/version.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/uv/unix.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/uv/threadpool.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/uv/linux.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/node.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/cppgc/common.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8config.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-array-buffer.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-local-handle.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-handle-base.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-internal.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8config.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-object.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-maybe.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-persistent-handle.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-weak-callback-info.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-primitive.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-data.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-value.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-traced-handle.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-container.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-context.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-snapshot.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-date.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-debug.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-script.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-callbacks.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-promise.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-message.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-exception.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-extension.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-external.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-function.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-function-callback.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-template.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-memory-span.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-initialization.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-isolate.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-embedder-heap.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-microtask.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-statistics.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-unwinder.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-embedder-state-scope.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-platform.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-source-location.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-json.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-locker.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-microtask-queue.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-primitive-object.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-proxy.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-regexp.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-typed-array.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-value-serializer.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-version.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/v8-wasm.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/node_version.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/node_buffer.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/node.h \
+ /home/vuongle/.electron-gyp/26.6.10/include/node/node_object_wrap.h \
+ ../../node_modules/nan/nan_callbacks.h \
+ ../../node_modules/nan/nan_callbacks_12_inl.h \
+ ../../node_modules/nan/nan_maybe_43_inl.h \
+ ../../node_modules/nan/nan_converters.h \
+ ../../node_modules/nan/nan_converters_43_inl.h \
+ ../../node_modules/nan/nan_new.h \
+ ../../node_modules/nan/nan_implementation_12_inl.h \
+ ../../node_modules/nan/nan_persistent_12_inl.h \
+ ../../node_modules/nan/nan_weak.h \
+ ../../node_modules/nan/nan_object_wrap.h \
+ ../../node_modules/nan/nan_private.h \
+ ../../node_modules/nan/nan_typedarray_contents.h \
+ ../../node_modules/nan/nan_json.h \
+ ../../node_modules/nan/nan_scriptorigin.h
+../src/unix/pty.cc:
+../../node_modules/nan/nan.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/node_version.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/uv.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/uv/errno.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/uv/version.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/uv/unix.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/uv/threadpool.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/uv/linux.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/node.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/cppgc/common.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8config.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-array-buffer.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-local-handle.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-handle-base.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-internal.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8config.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-object.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-maybe.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-persistent-handle.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-weak-callback-info.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-primitive.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-data.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-value.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-traced-handle.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-container.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-context.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-snapshot.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-date.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-debug.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-script.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-callbacks.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-promise.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-message.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-exception.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-extension.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-external.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-function.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-function-callback.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-template.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-memory-span.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-initialization.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-isolate.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-embedder-heap.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-microtask.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-statistics.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-unwinder.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-embedder-state-scope.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-platform.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-source-location.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-json.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-locker.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-microtask-queue.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-primitive-object.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-proxy.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-regexp.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-typed-array.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-value-serializer.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-version.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/v8-wasm.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/node_version.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/node_buffer.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/node.h:
+/home/vuongle/.electron-gyp/26.6.10/include/node/node_object_wrap.h:
+../../node_modules/nan/nan_callbacks.h:
+../../node_modules/nan/nan_callbacks_12_inl.h:
+../../node_modules/nan/nan_maybe_43_inl.h:
+../../node_modules/nan/nan_converters.h:
+../../node_modules/nan/nan_converters_43_inl.h:
+../../node_modules/nan/nan_new.h:
+../../node_modules/nan/nan_implementation_12_inl.h:
+../../node_modules/nan/nan_persistent_12_inl.h:
+../../node_modules/nan/nan_weak.h:
+../../node_modules/nan/nan_object_wrap.h:
+../../node_modules/nan/nan_private.h:
+../../node_modules/nan/nan_typedarray_contents.h:
+../../node_modules/nan/nan_json.h:
+../../node_modules/nan/nan_scriptorigin.h:
